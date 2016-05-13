044d476141b99578c32c84c0d28b4c695d82b8e2
Cody <codyloyd@gmail.com>
2016-05-13 11:55:45 -0500

    moved knight class, figured out how to get the gameboard into the knight class.. but now it seems to be giving me trouble because it doesn't like the gameboard being created so many times.

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,56 +1,53 @@
 class Chess
 	attr_accessor :gameboard
 	def initialize
-		create_gameboard
-		puts @gameboard.to_s
-		@knight= Knight.new([3,4])
 	end
 	
-	class Knight
-		attr_accessor :position, :possible_positions
-		def initialize(position)
-			@position = position
-			@legal_moves = [[1,2],[-1,2],[1,-2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]
-			calculate_possible_positions
-			knight_tree
-		end
-
-		def calculate_possible_positions
-		# creates an array of every possible next position 
-		# for an instance of a knight	
-			@possible_positions = []
-			@legal_moves.each do |m|
-				@possible_positions << [(self.position[0] + m[0]),(self.position[1]+m[1])]
-			end			
-		end
-
-		def knight_tree
-
-			self.possible_positions.map! do |p|
-				Knight.new(p) if @gameboard.include?(p)
-			end
-			puts self.possible_positions
-		end
-
-
-	end
-	
-	def create_gameboard
-		@gameboard = []
+	def gameboard
+		#generates a 2d array of spaces on the game board.
+		board = []
 		8.times do |i|
 			8.times do |x|
-				@gameboard << [i,x]
+				board << [i,x]
 			end
-		end				
+		end
+		return board				
 	end
-
-
-
-	# def knight_moves(start,final)
-	# 	@knight = Knight.new(start)
-	# end
-
 end
 
 
-chess = Chess.new
+class Knight < Chess
+	attr_accessor :position, :possible_positions
+	def initialize(position)
+		@position = position
+		#the list of moves that a knight can make.
+		@legal_moves = [[1,2],[-1,2],[1,-2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]
+		#keeps track of the positons that already have knights on them
+		@knights_on_board = [self.position] unless @knights_on_board
+		calculate_possible_positions		
+		knight_tree
+	end
+
+	def calculate_possible_positions
+	# creates an array of every possible next position 
+	# for an instance of a knight	
+		@possible_positions = []
+		@legal_moves.each do |m|
+			p = [(self.position[0] + m[0]),(self.position[1]+m[1])]
+			# makes sure that the possible position is a legal one (is included on gameboard)
+			@possible_positions << p if gameboard.include?(p)
+		end		
+	end
+
+	def knight_tree
+		#creates a new Knight at each possible positions
+		#ideally, recursively filling out the tree.
+		self.possible_positions.map! do |p|
+			Knight.new(p) unless @knights_on_board.include?(p)
+			@knights_on_board << p
+		end
+	end
+end
+
+
+knight = Knight.new([0,1])
