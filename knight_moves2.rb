da7c67a95f8450ae1851ef9160aeea49001c85af
Cody <codyloyd@gmail.com>
2016-05-13 19:44:36 -0500

    almost done.. implemented the creation of the knight_graph, and the search

--- knight_moves2.rb
+++ knight_moves2.rb
@@ -21,33 +21,46 @@ class Knight < Chess
 	def initialize(position)
 		@position = position
 		#the list of moves that a knight can make.
+		# -17,-15,-6,+10,15,-10,+6,17
 		@legal_moves = [[1,2],[-1,2],[1,-2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]
-		#keeps track of the positons that already have knights on them
-		@knights_on_board = [self.position] unless @knights_on_board
-		calculate_possible_positions		
-		knight_tree
+		make_knight_graph
+		BFS([3,4],[2,2])
 	end
 
-	def calculate_possible_positions
+	def calculate_possible_positions(position)
 	# creates an array of every possible next position 
 	# for an instance of a knight	
 		@possible_positions = []
 		@legal_moves.each do |m|
-			p = [(self.position[0] + m[0]),(self.position[1]+m[1])]
+			p = [(position[0] + m[0]),(position[1]+m[1])]
 			# makes sure that the possible position is a legal one (is included on gameboard)
-			@possible_positions << p if gameboard.include?(p)
+			@possible_positions << gameboard.index(p) if gameboard.include?(p)
+		end	
+		return @possible_positions
+	end
+
+	def make_knight_graph
+		@knight_graph = []
+		gameboard.each_with_index do |p,i|
+			@knight_graph << calculate_possible_positions(p)
 		end		
 	end
 
-	def knight_tree
-		#creates a new Knight at each possible positions
-		#ideally, recursively filling out the tree.
-		self.possible_positions.map! do |p|
-			Knight.new(p) unless @knights_on_board.include?(p)
-			@knights_on_board << p
+	def BFS(starting_position,final_position)
+		s = gameboard.index(starting_position)
+		f = gameboard.index(final_position)
+		q = [s]
+		until q.empty?
+			node = q.shift
+			# return node if node == f
+			puts "SUCCESS #{gameboard[node]}" if node == f
+			break if node == f
+			@knight_graph[node].each do |child|
+				q << child
+			end
 		end
 	end
+
 end
 
-
-knight = Knight.new([0,1])
+knight = Knight.new([3,5])
