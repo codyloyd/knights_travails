d082e1640cd8e5562406c3b438f7b4353ffac043
Cody <codyloyd@gmail.com>
2016-05-13 22:27:38 -0500

    I FREAKIN DID IT.. completely differently than anyone else that I know of.. oops

--- knight_moves2.rb
+++ knight_moves2.rb
@@ -18,18 +18,15 @@ end
 
 class Knight < Chess
 	attr_accessor :position, :possible_positions
-	def initialize(position)
+	def initialize(position = [0,0],parent=nil)
 		@position = position
-		#the list of moves that a knight can make.
-		# -17,-15,-6,+10,15,-10,+6,17
 		@legal_moves = [[1,2],[-1,2],[1,-2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]
 		make_knight_graph
-		BFS([3,4],[2,2])
+
 	end
 
 	def calculate_possible_positions(position)
 	# creates an array of every possible next position 
-	# for an instance of a knight	
 		@possible_positions = []
 		@legal_moves.each do |m|
 			p = [(position[0] + m[0]),(position[1]+m[1])]
@@ -40,27 +37,52 @@ class Knight < Chess
 	end
 
 	def make_knight_graph
+		#the knight graph is a graph of links from every position on the game board
 		@knight_graph = []
-		gameboard.each_with_index do |p,i|
+		gameboard.each do |p|
 			@knight_graph << calculate_possible_positions(p)
 		end		
 	end
 
-	def BFS(starting_position,final_position)
-		s = gameboard.index(starting_position)
-		f = gameboard.index(final_position)
-		q = [s]
-		until q.empty?
-			node = q.shift
-			# return node if node == f
-			puts "SUCCESS #{gameboard[node]}" if node == f
-			break if node == f
+	def BFS
+		search_queue = [@starting_position]
+		visited_nodes = []
+		path = []
+		until search_queue.empty?
+			node = search_queue.shift
+			visited_nodes << node
 			@knight_graph[node].each do |child|
-				q << child
+				path[child] = node if path[child] == nil
+				return path if child == @final_position
+				search_queue << child unless visited_nodes.include?(child) || search_queue.include?(child)
 			end
 		end
 	end
 
+	def find_shortest_path(path)
+		shortest_path = [@final_position]
+		loop do  
+			shortest_path << path[shortest_path[-1]]
+			break if shortest_path[-1] == @starting_position
+		end
+		shortest_path
+	end
+
+	def print_solution
+		puts "You made it in #{@solution_array.length - 1} moves!"
+		puts "Heres your path:"
+		@solution_array.reverse.each do |i|
+			puts gameboard[i].to_s
+		end
+	end
+
+	def knight_moves(starting_position,final_position)
+		@starting_position = gameboard.index(starting_position)
+		@final_position = gameboard.index(final_position)
+		@solution_array = find_shortest_path(BFS())
+		print_solution
+	end
 end
 
-knight = Knight.new([3,5])
+knight = Knight.new
+knight.knight_moves([3,3],[4,3])
